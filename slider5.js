eb9f355adf5bec7cfb4c34923c1bffb30df027f8
aniketbabariya24 <aniketbabariya24@gmail.com>
2023-07-10 11:34:05 +0530

    Project added

--- slider5.js
+++ slider5.js
@@ -4,34 +4,33 @@ let next5 = document.getElementById('next5');
 let prev5 = document.getElementById('prev5');
 let dots5 = document.querySelectorAll('.slider5 .dots5 li');
 
-let lengthItems = items5.length - 1;
+let lengthItems5 = items5.length - 1;
 let active5 = 0;
 next5.onclick = function(){
-    active5 = active5 + 1 <= lengthItems ? active5 + 1 : 0;
-    reloadSlider();
+    active5 = active5 + 1 <= lengthItems5 ? active5 + 1 : 0;
+    reloadSlider5();
 }
 prev5.onclick = function(){
-    active5 = active5 - 1 >= 0 ? active5 - 1 : lengthItems;
-    reloadSlider();
+    active5 = active5 - 1 >= 0 ? active5 - 1 : lengthItems5;
+    reloadSlider5();
 }
 
-function reloadSlider(){
+function reloadSlider5(){
     slider5.style.left = -items5[active5].offsetLeft + 'px';
     // 
     let last_active5_dot = document.querySelector('.slider5 .dots5 li.active5');
     last_active5_dot.classList.remove('active5');
     dots5[active5].classList.add('active5');
 
-    clearInterval(refreshInterval);
     
 }
 
 dots5.forEach((li, key) => {
     li.addEventListener('click', ()=>{
          active5 = key;
-         reloadSlider();
+         reloadSlider5();
     })
 })
 window.onresize = function(event) {
-    reloadSlider();
+    reloadSlider5();
 };
